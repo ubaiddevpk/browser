5ca298de14a4881cb5ce73c3ca06ec149eabd662
Obaid ullah <168396695+ubaiddevpk@users.noreply.github.com>
2024-11-02 14:29:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function next(url) {
+  window.location.assign(url)
+}
+function relode(){
+ window.location.reload()
+}
+function back(){
+  window.history.back();
+}
+function farward(){
+  window.history.forward();
+}
+function open1(){
+ let win= window.open('https://www.linkedin.com/','','width=500,height=500');
+}
+function close1(){
+  window.close(win);
+}
